60b4472964587415227e4de20ea741dba9367ff6
Andres Fernandez <andres-carlos.fernandez-baker@oclock.school>
2024-02-21 20:44:50 +0100

    mods to post route for handling form submission from front, functional

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Project;
 use App\Form\ProjectType;
 use App\Repository\ProjectRepository;
-use App\Service\ProjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -36,7 +35,7 @@ class ProjectController extends AbstractController {
     /**
      * @Route("/projects", methods={"POST"})
      */
-    public function postProject(SerializerInterface $serializer, Request $request): JsonResponse
+    public function postProject(ProjectRepository $projectRepository, Request $request): JsonResponse
     {
         //! no longer need the project manager with the database setup
         // $project = $this->projectManager->createProject();
@@ -49,12 +48,15 @@ class ProjectController extends AbstractController {
 
         $project = new Project();
         $form = $this->createForm(ProjectType::class, $project);
+        $data = json_decode($request->getContent(), true);
         $form->handleRequest($request);
+        $form->submit($data);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            //! replaced $project->serialize with serializer interface
+        if ($form->isValid()) {
+            //! replaced $project->serialize with repository flush and return of new $project object
             // return $this->json($project->serialize());
-            return $serializer->serialize($project, 'json');
+            $projectRepository->add($project, true);
+            return $this->json($project, Response::HTTP_CREATED);
         }
 
         return $this->json(["error" => "An error occurred"]);
